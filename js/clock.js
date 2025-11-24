447442fa886994425f388ddd9788ae28c966f110
tranhuuloc2010 <tranhuuloc2010@gmail.com>
2025-11-24 19:14:01 +0700

    Update time tan hon

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2025-11-23 08:30", "Asia/Kolkata");
+  let targetDate = moment.tz("2025-11-30 10:30", "Asia/Kolkata");
 
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
